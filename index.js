505b7c48661aecaeb78a30063da05973a020634b
Joel De Conto <joel_d.c@hotmail.com>
2022-11-15 15:07:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
     app.use(cors());
     next();
